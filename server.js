7b56d54494bc25de1dc74b07fe0d1cf7103c319c
Kyle Easton <kyleeaston@Kyles-MacBook-Air.local>
2017-12-13 15:08:11 -0800

    db connection for delete

--- server.js
+++ server.js
@@ -48,6 +48,9 @@ app.get('/api/books', function (req, res) {
 app.get('/api/books/:id', function (req, res) {
   // find one book by its id
   db.Book.find({_id: req.params.id}, function(err, books) {
+    if (err) {
+      res.json(err);
+    }
     res.json(books);
   });
 });
@@ -56,8 +59,10 @@ app.get('/api/books/:id', function (req, res) {
 // create new book
 app.post('/api/books', function (req, res) {
   // create new book with form data (`req.body`)
-  console.log(req.body)
   db.Book.create(req.body, function (err, books) {
+    if (err) {
+      res.json(err);
+    }
     res.json(books);
   });
 });
@@ -66,41 +71,34 @@ app.post('/api/books', function (req, res) {
 app.put('/api/books/:id', function(req,res){
 // get book id from url params (`req.params`)
   db.Book.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, books) {
+    if (err) {
+      res.json(err);
+    }
     res.json(books);
   });
 });
 
-
-
-
-//   console.log('books update', req.params);
-//   var bookId = req.params.id;
-//   // find the index of the book we want to remove
-//   var updateBookIndex = books.findIndex(function(element, index) {
-//     return (element._id === parseInt(req.params.id)); //params are strings
-//   });
-//   console.log('updating book with index', deleteBookIndex);
-//   var bookToUpdate = books[deleteBookIndex];
-//   books.splice(updateBookIndex, 1, req.params);
-//   res.json(req.params);
-// });
-
-
-
 // delete book
 app.delete('/api/books/:id', function (req, res) {
-  // get book id from url params (`req.params`)
-  console.log('books delete', req.params);
-  var bookId = req.params.id;
-  // find the index of the book we want to remove
-  var deleteBookIndex = books.findIndex(function(element, index) {
-    return (element._id === parseInt(req.params.id)); //params are strings
+  db.Book.findByIdAndRemove(req.params.id, function (err, books) {
+    if (err) {
+      res.json(err);
+    }
+    res.json(books);
   });
-  console.log('deleting book with index', deleteBookIndex);
-  var bookToDelete = books[deleteBookIndex];
-  books.splice(deleteBookIndex, 1);
-  res.json(bookToDelete);
 });
+//   // get book id from url params (`req.params`)
+//   console.log('books delete', req.params);
+//   var bookId = req.params.id;
+//   // find the index of the book we want to remove
+//   var deleteBookIndex = books.findIndex(function(element, index) {
+//     return (element._id === parseInt(req.params.id)); //params are strings
+//   });
+//   console.log('deleting book with index', deleteBookIndex);
+//   var bookToDelete = books[deleteBookIndex];
+//   books.splice(deleteBookIndex, 1);
+//   res.json(bookToDelete);
+// });
 
 
 
