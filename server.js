a8d0d0fd1b645a8be5b20223eea0dc2ae59499f4
Kyle Easton <kyleeaston@Kyles-MacBook-Air.local>
2017-12-13 13:40:17 -0800

    removed hard coded books array. added new get for all books

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
 
 //require express in our app
 var express = require('express'),
-  bodyParser = require('body-parser');
+    bodyParser = require('body-parser');
 
 // generate a new express app and call it 'app'
 var app = express();
@@ -19,38 +19,12 @@ app.use(express.static('public'));
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var db = require ('./models');
 
 
 ////////////////////
 //  DATA
 ///////////////////
-
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
-
-
-var newBookUUID = 18;
 
 
 
@@ -70,11 +44,16 @@ app.get('/', function (req, res) {
   res.sendFile('views/index.html' , { root : __dirname});
 });
 
-// get all books
+
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  console.log('books index');
-  res.json(books);
+  db.Book.find(function(err, books){
+    if (err) {
+      console.log("index error: " + err);
+      res.sendStatus(500);
+    }
+    res.json(books);
+  });
 });
 
 // get one book
